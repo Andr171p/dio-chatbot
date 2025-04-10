88619746effa725de5a5bbc9e11c75410cfe0497
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-04-10 17:57:46 +0500

    10.04.2025 refactor sqlite script

--- scripts/create_sqlite_db.py
+++ scripts/create_sqlite_db.py
@@ -1,9 +1,11 @@
 import sqlite3
+from pathlib import Path
 
-from src.settings import BASE_DIR
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 db_path = BASE_DIR / "db.sqlite3"
 
 
-connection = sqlite3.connect(db_path)
+if __name__ == "__main__":
+    connection = sqlite3.connect(db_path)
